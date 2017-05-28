51a13ec7424aa12aff58bb28b41180c58234e280
linwenjun <linwenjun@163.com>
2017-05-28 23:13:58 +0800

    add intergration-test

---
+++ src/integration-test/java/cn/wenjun/learning/AppTest.java
@@ -0,0 +1,27 @@
+package cn.wenjun.learning;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoad() throws Exception {
+        Assertions.assertThat(this.restTemplate.getForObject("http://localhost:" + port +"/blogs", String.class))
+                .contains("first");
+    }
+}
