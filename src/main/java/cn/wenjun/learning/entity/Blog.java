2d8924bb055349a61e600e9a2357720a0c7ddd60
TW-Recruiting-System <twer@TW-Recruiting-Systems-Mac-mini.local>
2017-06-21 16:48:35 +0800

    add lombok for simlefy the bean

--- src/main/java/cn/wenjun/learning/entity/Blog.java
+++ src/main/java/cn/wenjun/learning/entity/Blog.java
@@ -1,42 +1,11 @@
 package cn.wenjun.learning.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
-public class Blog {
+public @Data class Blog {
     private int id;
     private String name;
     private Date updateAt;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Blog{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", updateAt=" + updateAt +
-                '}';
-    }
 }
